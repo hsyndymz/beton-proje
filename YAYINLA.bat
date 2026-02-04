efb04e2ca56162cedec0f4b343394b11ec2f4d7f
hsyndymz <hsyndymz21@gmail.com>
2026-02-04 11:26:07 +0300

    Delete YAYINLA.bat

--- YAYINLA.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo --- BETON PROJE OTOMATIK YUKLEYICI ---
-git add .
-set /p msg="Guncelleme Notu (Bos birakilabilir): "
-if "%msg%"=="" set msg="Guncelleme: %date% %time%"
-git commit -m "%msg%"
-git push origin main
-echo --- YUKLEME TAMAMLANDI! BULUT 2 DK ICINDE GUNCELENECEKTIR. ---
-pause
